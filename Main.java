4d18ba09a121c0c028ff0cb4ece278f5429e31d9
natysls <59856222+natysls@users.noreply.github.com>
2020-03-09 15:58:02 -0300

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        Calendario c1 = new Calendario(01, 01, 2020);
+        Calendario c2 = new Calendario(02, 01, 2020);
+        c1.toString();
+        c2.toString();
+
+
+    }
+
+}
